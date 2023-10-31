95ddb1a78345fbc76f596bf42fe858c59e14d4f0
development <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-10-31 07:21:49 -0400

    building Domain structure

--- src/Modules/CloudSuite.Domain/Models/Media.cs
+++ src/Modules/CloudSuite.Domain/Models/Media.cs
@@ -1,6 +1,13 @@
+using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CloudSuite.Domain.Models
 {
-    public class Media
+    public class Media : Entity, IAggregateRoot
     {
         
     }
