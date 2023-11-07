0aa78426e57105dd23a1ccc9762c02b1e003d7d6
Lester <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-11-07 17:04:58 -0500

    Buidling Data repositories on Core microservice

--- src/CloudSuite.Infrastructure/Repositories/AddressRepository.cs
+++ src/CloudSuite.Infrastructure/Repositories/AddressRepository.cs
@@ -1,7 +1,56 @@
+using System.Data.Common;
+using CloudSuite.Domain.Contracts;
+using CloudSuite.Domain.Models;
+using CloudSuite.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace CloudSuite.Infrastructure.Repositories
 {
-    public class AddressRepository
+    public class AddressRepository : IAddressRepository
     {
         
+        protected readonly CoreDbContext Db;
+        protected readonly DbSet<Address> DbSet;
+        
+        public AddressRepository(CoreDbContext context)
+        {
+            Db = context;
+            DbSet = context.Addresses;
+        }
+
+        
+
+        public async Task Add(Address address)
+        {
+            await Task.Run(() => {
+                DbSet.Add(address);
+                Db.SaveChanges();
+            });
+        }
+
+        public async Task<Address> GetByAddressLine(string AddressLine1)
+        {
+            return await DbSet.AsNoTracking().FirstOrDefaultAsync(c => c.AddressLine == accountDigit);
+        }
+
+        public async Task<Address> GetByContactName(string contactName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Address>> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Address address)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
