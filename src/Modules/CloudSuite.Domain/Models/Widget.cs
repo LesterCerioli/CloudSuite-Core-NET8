016d3d5bf2ca3484c73cbbaaec4a400dc7a0b59d
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-09 17:21:05 -0300

    "feat(#1254): updating entity"

---
+++ src/Modules/CloudSuite.Domain/Models/Widget.cs
@@ -0,0 +1,14 @@
+﻿using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Domain.Models
+{
+    public class Widget : Entity, IAggregateRoot
+    {
+
+    }
+}
