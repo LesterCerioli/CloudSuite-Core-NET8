5257c21d72f0a00ec5a9c817cbfc396d0dec5bc9
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-11 09:22:07 -0300

    "feat(#1254): building Country handlers"

---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Country/Request/CheckCountryExistsByCodeRequest.cs
@@ -0,0 +1,25 @@
+﻿using CloudSuite.Modules.Application.Handlers.Country.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Handlers.Country.Request
+{
+    public class CheckCountryExistsByCodeRequest : IRequest<CheckCountryExistsByCodeResponse>
+    {
+        public Guid Id { get; set; }
+
+        public string? Code3 { get; private set; }
+
+        public CheckCountryExistsByCodeRequest(string code3)
+        {
+            Id = Guid.NewGuid();
+            Code3 = code3;
+        }
+
+        public CheckCountryExistsByCodeRequest() { }
+    }
+}
---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Country/Request/CheckCountryExistsByCountryNameRequest.cs
@@ -0,0 +1,26 @@
+﻿using CloudSuite.Domain.ValueObjects;
+using CloudSuite.Modules.Application.Handlers.Country.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Handlers.Country.Request
+{
+    public class CheckCountryExistsByCountryNameRequest : IRequest<CheckCountryExistsByCountryNameResponse>
+    {
+        public Guid Id { get; set; }
+
+        public string? CountryName { get; private set; }
+
+        public CheckCountryExistsByCountryNameRequest(string countryName)
+        {
+            Id = Guid.NewGuid();
+            CountryName = countryName;
+        }
+
+        public CheckCountryExistsByCountryNameRequest() { }
+    }
+}
