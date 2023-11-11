813f0825176521cdb9c54ecb0068a5c1d637ddb8
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-11 09:21:08 -0300

    "feat(#1254): building address handlers"

---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Address/CheckAddressExistsByContactNameHandler.cs
@@ -0,0 +1,47 @@
+﻿using CloudSuite.Domain.Contracts;
+using CloudSuite.Modules.Application.Hadlers.Address.Requests;
+using CloudSuite.Modules.Application.Hadlers.Address.Responses;
+using CloudSuite.Modules.Application.Validations.Address;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Hadlers.Address
+{
+    public class CheckAddressExistsByContactNameHandler : IRequestHandler<CheckAddressExistsByContactNameRequest, CheckAddressExistsByContactNameResponse>
+    {
+        private IAddressRepository _addressRepository;
+        private readonly ILogger<CheckAddressExistsByAddressLineHandler> _logger;
+
+
+        public async Task<CheckAddressExistsByContactNameResponse> Handle(CheckAddressExistsByContactNameRequest request, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"CheckExtractExistsByDateRequest: {JsonSerializer.Serialize(request)}");
+            var validationResult = new CheckAddressExistsByContactNameRequestValidation().Validate(request);
+
+            if (validationResult.IsValid)
+            {
+                try
+                {
+                    var contactName = await _addressRepository.GetByContactName(request.ContactName);
+
+                    if (contactName != null)
+                    {
+                        return await Task.FromResult(new CheckAddressExistsByContactNameResponse(request.Id, true, validationResult));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogCritical(ex.Message);
+                    return await Task.FromResult(new CheckAddressExistsByContactNameResponse(request.Id, "Failed to process the request."));
+                }
+            }
+            return await Task.FromResult(new CheckAddressExistsByContactNameResponse(request.Id, false, validationResult));
+        }
+    }
+}
---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Address/Requests/CheckAddressExistsByContactNameRequest.cs
@@ -0,0 +1,25 @@
+﻿using CloudSuite.Modules.Application.Hadlers.Address.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Hadlers.Address.Requests
+{
+    public class CheckAddressExistsByContactNameRequest : IRequest<CheckAddressExistsByContactNameResponse>
+    {
+        public Guid Id { get; private set; }
+
+        public string? ContactName { get; private set; }
+
+        public CheckAddressExistsByContactNameRequest(string contactName)
+        {
+            Id = Guid.NewGuid();
+            ContactName = contactName;
+        }
+
+        public CheckAddressExistsByContactNameRequest() { }
+    }
+}
