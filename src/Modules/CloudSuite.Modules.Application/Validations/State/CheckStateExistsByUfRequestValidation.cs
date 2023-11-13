add6184ef024fb3d451ca19c356f5e7efe15d3c6
igums <igormsantos1@icloud.com>
2023-11-13 08:51:24 -0300

    "feat(#1254) building core validations"

--- src/Modules/CloudSuite.Modules.Application/Validations/State/CheckStateExistsByUfRequestValidation.cs
+++ src/Modules/CloudSuite.Modules.Application/Validations/State/CheckStateExistsByUfRequestValidation.cs
@@ -10,6 +10,16 @@ namespace CloudSuite.Modules.Application.Validations.State
 {
     public class CheckStateExistsByUfRequestValidation : AbstractValidator<CheckStateExistsByUfRequest>
     {
-
+        public CheckStateExistsByUfRequestValidation ()
+        {
+            RuleFor(a => a.UF)
+                .NotEmpty()
+                .WithMessage("O campo UF é obrigatório.")
+                .Length(2)
+                .Matches(@"^[A-Z]{2}$")
+                .WithMessage("O campo UF deve conter exatamente 2 letras maiúsculas.")
+                .NotNull()
+                .WithMessage("O campo UF não pode ser nulo.");
+        }
     }
 }
