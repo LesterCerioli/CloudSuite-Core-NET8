3e56351641ab495bf97a50db1bb58ff4f784ddf6
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-12-14 21:23:13 -0300

    feat(#1145): building tests

---
+++ test/xunit/CloudSuite.Modules.Application.Tests/Services/AddressAppServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CloudSuite.Domain.Contracts;
+using CloudSuite.Modules.Application.Services.Implementations;
+using NetDevPack.Mediator;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Tests.Services
+{
+    public class AddressAppServiceTests
+    {
+
+        public async Task GetByAddressLine1_ShouldReturnsCompanyViewModel(string addressLine, string contactName)
+        {
+            var addressRepositoryMock = new Mock<IAddressRepository>();
+            var mediatorHandlerMock = new Mock<IMediatorHandler>();
+            var mapperMock = new Mock<IMapper>();
+
+            var tomadorServicoAppService = new AddressAppService(
+                addressRepositoryMock.Object,
+                mediatorHandlerMock.Object,
+                mapperMock.Object);
+        }
+    }
+}
