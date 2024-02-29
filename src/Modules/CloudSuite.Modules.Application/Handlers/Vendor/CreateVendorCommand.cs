b44e23fa0565efcd51ab2db8757538cf2ff4c813
Lucas Barros <lucas.barros@lucastechnologyservice.com>
2024-02-29 18:05:13 +0000

    feat(#1343): refactoring vendor handlers

--- src/Modules/CloudSuite.Modules.Application/Handlers/Vendor/CreateVendorCommand.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Vendor/CreateVendorCommand.cs
@@ -1,46 +1,42 @@
-﻿using CloudSuite.Domain.ValueObjects;
+﻿using CloudSuite.Domain.Models;
+using CloudSuite.Domain.ValueObjects;
 using CloudSuite.Modules.Application.Handlers.Vendor.Responses;
 using MediatR;
 using System.ComponentModel.DataAnnotations;
 using VendorEntity = CloudSuite.Domain.Models.Vendor;
 using EmailEntity = CloudSuite.Domain.Models.Email;
-using UserEntity = CloudSuite.Domain.Models.User;
 
 namespace CloudSuite.Modules.Application.Handlers.Vendor
 {
     public class CreateVendorCommand : IRequest<CreateVendorResponse>
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
         [StringLength(450)]
-        public string? Name { get; private set; }
+        public string? Name { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
         [StringLength(450)]
-        public string? Slug { get; private set; }
+        public string? Slug { get; set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
         [StringLength(100)]
-        public string? Description { get; private set; }
+        public string? Description { get; set; }
 
-        public Cnpj Cnpj { get; private set; }
+        public string? Cnpj { get; set; }
 
-        public Guid CnpjId { get; private set; }
+        public EmailEntity Email { get; set; }
 
-        public EmailEntity Email { get; private set; }
+        public Guid EmailId {  get; set; }
 
-        public Guid EmailId { get; private set; }
+        public DateTimeOffset? CreatedOn { get; set; }
 
-        public DateTimeOffset? CreatedOn { get; private set; }
+        public DateTimeOffset? LatestUpdatedOn { get; set; }
 
-        public DateTimeOffset? LatestUpdatedOn { get; private set; }
+        public bool? IsActive { get; set; }
 
-        public bool? IsActive { get; private set; }
-
-        public bool? IsDeleted { get; private set; }
-
-        public UserEntity User { get; private set; }
+        public bool? IsDeleted { get; set; }
 
         public Guid UserId { get; private set; }
 
@@ -53,12 +49,12 @@ namespace CloudSuite.Modules.Application.Handlers.Vendor
                 this.Name,
                 this.Slug,
                 this.Description,
-                this.Cnpj,
-                this.Email,
+                new Cnpj(this.Cnpj),
+                this.EmailId,
                 this.CreatedOn,
                 this.LatestUpdatedOn,
-                this.IsActive,
-                this.IsDeleted
+                this.IsActive.Value,
+                this.IsDeleted.Value
                 );
         }
     }
