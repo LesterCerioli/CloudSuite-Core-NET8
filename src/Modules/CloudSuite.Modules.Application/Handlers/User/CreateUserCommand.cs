01c7fb6861932266e1b3d498a1fb9e327980f739
Lucas Barros <lucas.barros@lucastechnologyservice.com>
2024-02-29 18:14:48 +0000

    feat(#1343): refactoring user handlers

--- src/Modules/CloudSuite.Modules.Application/Handlers/User/CreateUserCommand.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/User/CreateUserCommand.cs
@@ -12,45 +12,42 @@ namespace CloudSuite.Modules.Application.Handlers.User
     public class CreateUserCommand : IRequest<CreateUserResponse>
     {
 
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
 
         [Required(ErrorMessage = "The {0} field is required.")]
-        public string? FullName { get; private set; }
+        public string? FullName { get; set; }
 
-        public EmailEntity Email { get; private set; }
+        public EmailEntity Email { get; set; }
 
-        public Cpf Cpf { get; private set; }
+        public string? Cpf { get; set; }
 
-        public Telephone Telephone { get; private set; }
+        public string? Telephone { get; set; }
 
-        public VendorEntity Vendor { get; private set; }
+        public VendorEntity Vendor { get; set; }
 
-        public bool? IsDeleted { get; private set; }
+        public bool? IsDeleted { get; set; }
 
-        public DateTimeOffset? CreatedOn { get; private set; }
+        public DateTimeOffset? CreatedOn { get; set; }
 
-        public DateTimeOffset? LatestUpdatedOn { get; private set; }
-
-        [StringLength(450)]
-        public string? RefreshTokenHash { get; private set; }
+        public DateTimeOffset? LatestUpdatedOn { get; set; }
 
         [StringLength(450)]
-        public string? Culture { get; private set; }
+        public string? RefreshTokenHash { get; set; }
 
-        public string? ExtensionData { get; private set; }
+        [StringLength(450)]
+        public string? Culture { get; set; }
 
-        public Guid VendorId { get; private set; }
+        public string? ExtensionData { get; set; }
+
+        public Guid VendorId { get; set; }
 
 
         public UserEntity GetEntity()
         {
             return new UserEntity(
                 this.FullName,
-                this.Email,
-                this.Cpf,
-                this.Telephone,
-                this.Vendor,
-                this.IsDeleted,
+                new Cpf(this.Cpf),
+                this.IsDeleted.Value,
                 this.CreatedOn,
                 this.LatestUpdatedOn,
                 this.RefreshTokenHash,
