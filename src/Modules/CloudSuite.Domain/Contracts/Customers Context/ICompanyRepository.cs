9e70da4b22e9b9dbf27a9f3d7500217d7597994c
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-03-11 19:03:23 -0300

    feat(#1367): building Entity models and Domain contract for Customer context

--- src/Modules/CloudSuite.Domain/Contracts/Customers Context/ICompanyRepository.cs
+++ src/Modules/CloudSuite.Domain/Contracts/Customers Context/ICompanyRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CloudSuite.Domain.Models;
+using CloudSuite.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,18 @@ namespace CloudSuite.Domain.Contracts.Customers_Context
 {
 	public interface ICompanyRepository
 	{
+		Task<Company> GetByCnpj(Cnpj cnpj);
+
+		Task<Company> GetBySocialName(string socialName);
+
+		Task<Company> GetByFantasyName(string fantasyName);
+
+		Task<IEnumerable<Company>> GetCompanies();
+
+		Task Add(Company company);
+
+		void Updater(Company company);	
+
+		void Remove(Company company);
 	}
 }
