f103ab07275f999a341b2ead6ca1452ed26a6308
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-03-11 20:56:01 -0300

    feature(#1370): Building EF Core and dapper mapping and subscripting table on Db Context class

---
+++ src/CloudSuite.Infrastructure/Mappings/Dapper/PasswordGenerator/PasswordDapperMapping.cs
@@ -0,0 +1,42 @@
+﻿using CloudSuite.Domain.Models.Password_Generator_Context;
+using CloudSuite.Infrastructure.Context;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Infrastructure.Mappings.Dapper.PasswordGenerator
+{
+	public class PasswordDapperMapping
+	{
+
+
+		private readonly IDbConnection _connection;
+
+		public CoreDbContext CoreDbContext { get; set; }
+
+		public PasswordDapperMapping(CoreDbContext context)
+		{
+			CoreDbContext = context;
+			_connection = context.Database.GetDbConnection();
+		}
+
+
+
+		public async Task<IEnumerable<Password>> GetAllPasswordAsync()
+		{
+			var query = @"
+				SELECT
+					Id,
+					Caracter,
+					CreateOn
+				FROM
+					Passwords";
+			return await _connection.QueryAsync<Password>(query);
+		}
+	}
+}
--- src/CloudSuite.Infrastructure/Repositories/PasswordGeneratorContext/PasswordRepository.cs
+++ src/CloudSuite.Infrastructure/Repositories/PasswordGeneratorContext/PasswordRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CloudSuite.Domain.Contracts.Password_Generator_Context;
+using CloudSuite.Domain.Models.Password_Generator_Context;
+using CloudSuite.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,55 @@ using System.Threading.Tasks;
 
 namespace CloudSuite.Infrastructure.Repositories.PasswordGeneratorContext
 {
-	public class PasswordRepository
+	public class PasswordRepository : IPasswordRepository
 	{
+
+		protected readonly CoreDbContext Db;
+		protected readonly DbSet<Password> DbSet;
+
+		public PasswordRepository(CoreDbContext context)
+		{
+			Db = context;
+			DbSet = context.Passwords;
+		}
+
+		public async Task Add(Password password)
+		{
+			await Task.Run(() =>
+			{
+				DbSet.Add(password);
+				Db.SaveChangesAsync();
+			});
+		}
+
+		public async Task<Password> GetByCaracter(int caracterNumber)
+		{
+			return await DbSet.FirstOrDefaultAsync(c => c.CaracterNumber == caracterNumber);
+		}
+
+		public async Task<Password> GetBySecret(string senha)
+		{
+			return await DbSet.FirstOrDefaultAsync(c => c.Senha == senha);
+		}
+
+		public async Task<IEnumerable<Password>> GetList()
+		{
+			return await DbSet.ToListAsync();
+		}
+
+		public void Remove(Password password)
+		{
+			DbSet?.Remove(password);
+		}
+
+		public void Update(Password password)
+		{
+			DbSet.Update(password);
+		}
+
+		public void Dispose()
+		{
+			Db.Dispose();
+		}
 	}
 }
