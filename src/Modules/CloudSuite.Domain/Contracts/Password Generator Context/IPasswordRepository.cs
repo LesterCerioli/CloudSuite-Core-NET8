3054f0562ef6aeaed5d35c12302a1900d67993ec
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-03-11 18:29:09 -0300

    feat(#1368): Bringing Entity models

--- src/Modules/CloudSuite.Domain/Contracts/Password Generator Context/IPasswordRepository.cs
+++ src/Modules/CloudSuite.Domain/Contracts/Password Generator Context/IPasswordRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CloudSuite.Domain.Models.Password_Generator_Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace CloudSuite.Domain.Contracts.Password_Generator_Context
 {
 	public interface IPasswordRepository
 	{
+		Task<Password> GetByCaracter(int caracterNumber);
+
+		Task<Password> GetBySecret(string senha);
+
+		Task<IEnumerable<Password>> GetList();
+		
+		Task Add(Password password);
+
+		void Update(Password password);
+
+		void Remove(Password password);
 	}
 }
