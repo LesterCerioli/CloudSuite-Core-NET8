1d63138c2eb7eae69604cb80b8b51926743c6923
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-03-13 17:31:46 -0300

    feat(#1267): Fixing Controllers

--- src/CloudSuite.Infrastructure.CrossCutting/DependencyInjector/MediatorServiceCollectionExtension.cs
+++ src/CloudSuite.Infrastructure.CrossCutting/DependencyInjector/MediatorServiceCollectionExtension.cs
@@ -9,20 +9,20 @@ using System.Threading.Tasks;
 
 namespace CloudSuite.Infrastructure.CrossCutting.DependencyInjector
 {
-	public static class MediatorServiceCollectionExtension
-	{
-		public static IServiceCollection AddMediator(this IServiceCollection services)
-		{
-			var assembly = AppDomain.CurrentDomain.Load("CloudSuite.Modules.Application");
+    public static class MediatorServiceCollectionExtension
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services)
+        {
+            var assembly = AppDomain.CurrentDomain.Load("CloudSuite.Modules.Application");
 
-			AssemblyScanner
-				.FindValidatorsInAssembly(assembly)
-				.ForEach(result => services.AddScoped(result.InterfaceType, result.ValidatorType));
+            AssemblyScanner
+                .FindValidatorsInAssembly(assembly)
+                .ForEach(result => services.AddScoped(result.InterfaceType, result.ValidatorType));
 
-			//services.AddScoped(typeof(IPipelineBehavior<,>), typeof(FailFastRequestBehavior<,>));
+            //services.AddScoped(typeof(IPipelineBehavior<,>), typeof(FailFastRequestBehavior<,>));
 
-			services.AddMediatR(assembly);
-			return services;
-		}
-	}
+            services.AddMediatR(assembly);
+            return services;
+        }
+    }
 }
--- src/Rest/CloudSuite.Services.Core.API/Program.cs
+++ src/Rest/CloudSuite.Services.Core.API/Program.cs
@@ -1,102 +1,21 @@
-using AutoMapper;
-using CloudSuite.Domain.Contracts;
-using CloudSuite.Domain.Contracts.PasswordGeneratorContext;
-using CloudSuite.Infrastructure.Context;
-using CloudSuite.Infrastructure.CrossCutting.DependencyInjector;
-using CloudSuite.Infrastructure.CrossCutting.HealthChecks;
-using CloudSuite.Infrastructure.CrossCutting.Middlewares;
-using CloudSuite.Infrastructure.Repositories;
-using CloudSuite.Infrastructure.Repositories.PasswordGeneratorContext;
-using CloudSuite.Modules.Application.Services.Contracts;
-using CloudSuite.Modules.Application.Services.Contracts.PasswirdGeneratorContext;
-using CloudSuite.Modules.Application.Services.Implementations;
-using CloudSuite.Modules.Application.Services.Implementations.PasswordGeneratorContext;
-using CloudSuite.Services.Core.API.Configurations;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
-using NetDevPack.Mediator;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<CoreDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONECTIONSTRING")));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddMediator();
-builder.Services.AddLogger();
-//builder.Services.AddTwilioServices();
-builder.Services.AddHealthCheckConfigurations();
-builder.Services.AddSwaggerDocVersion();
-builder.Services.AddTransient<ExceptionHandlingMiddleware>();
-builder.Services.AddDatabaseConfiguration(builder.Configuration);
-builder.Services.AddDbContext<CoreDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONECTIONSTRING")));
+builder.Services.AddSwaggerGen();
 
-
-var configuration = new MapperConfiguration(cfg =>
-{
-});
-
-
-
-builder.Services.AddTransient<IAddressAppService, AddressAppService>();
-builder.Services.AddTransient<ICityAppService, CityAppService>();
-builder.Services.AddTransient<ICompanyAppService, CompanyAppService>();
-builder.Services.AddTransient<ICountryAppService, CountryAppService>();
-builder.Services.AddTransient<IDistrictAppService, DistrictAppService>();
-builder.Services.AddTransient<IEmailAppService, EmailAppService>();
-builder.Services.AddTransient<IMediaAppService, MediaAppService>();
-builder.Services.AddTransient<IStateAppService, StateAppService>();
-builder.Services.AddTransient<IUserAppService, UserAppService>();
-builder.Services.AddTransient<IVendorAppService, VendorAppService>();
-builder.Services.AddTransient<IPasswordAppService, PasswordAppService>();
-builder.Services.AddTransient<IMediator, Mediator>();
-builder.Services.AddTransient<IAddressRepository, AddressRepository>();
-builder.Services.AddTransient<ICityRepository, CityRepository>();
-builder.Services.AddTransient<ICompanyRepository, CompanyRepository>();
-builder.Services.AddTransient<ICountryRepository, CountryRepository>();
-builder.Services.AddTransient<IDistrictRepository, DistrictRepository>();
-builder.Services.AddTransient<IEmailRepository, EmailRepository>();
-builder.Services.AddTransient<IMediaRepository, MediaRepository>();
-builder.Services.AddTransient<IStateRepository, StateRepository>();
-builder.Services.AddTransient<IUserRepository, UserRepository>();
-builder.Services.AddTransient<IVendorRepository, VendorRepository>();
-builder.Services.AddTransient<IPasswordRepository, PasswordRepository>();
-builder.Services.AddTransient<IMediatorHandler, MediatorHandler>();
-builder.Services.AddSingleton<IMapper>(configuration.CreateMapper());
-
-builder.Services.AddCors(options =>
-{
-	options.AddPolicy("my-cors",
-						  policy =>
-						  {
-							  policy
-							  .AllowAnyOrigin()
-							  .AllowAnyHeader()
-							  .AllowAnyMethod();
-						  });
-});
 var app = builder.Build();
 
-
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
-	app.UseHttpLogging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-//app.UseMiddleware<ExceptionHandlingMiddleware>();
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
-app.UseCors(x => x
-.AllowAnyOrigin()
-.AllowAnyMethod()
-.AllowAnyHeader()
-);
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -104,9 +23,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-
-
-
-
