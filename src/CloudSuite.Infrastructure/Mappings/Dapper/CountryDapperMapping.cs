b8885efb09f6fa964cb83ebc2a2a707c06893be2
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-04-13 23:32:53 -0300

    feat(#1435): Building Dapper queries

--- src/CloudSuite.Infrastructure/Mappings/Dapper/CountryDapperMapping.cs
+++ src/CloudSuite.Infrastructure/Mappings/Dapper/CountryDapperMapping.cs
@@ -1,25 +1,51 @@
 using CloudSuite.Domain.Models;
+using Dapper;
 using Dapper.FluentMap.Dommel.Mapping;
+using System.Data;
 
 namespace CloudSuite.Infrastructure.Mappings.Dapper
 {
-    public class CountryDapperMapping : DommelEntityMap<Country>
+    public class CountryDapperMapping
     {
-        public CountryDapperMapping()
+        private readonly IDbConnection _connection;
+        public CountryDapperMapping(IDbConnection connection)
         {
-            ToTable("Countries");
-            Map(p => p.Id).IsKey();
-            Map(p => p.CountryName).ToColumn("CountryName");
-            Map(p => p.Code3).ToColumn("Code");
-            Map(p => p.IsBillingEnabled).ToColumn("IsBillingEnabled");
-            Map(p => p.IsShippingEnabled).ToColumn("IsShippingEnabled");
-            Map(p => p.IsCityEnabled).ToColumn("IsCityEnabled");
-            Map(p => p.IsZipCodeEnabled).ToColumn("IsZipCodeEnabled");
-            Map(p => p.IsDistrictEnabled).ToColumn("IsDistrictEnabled");
-            
+            _connection = connection;
+        }
 
-            // Mapping for a relationship
-            Map(p => p.StateId).ToColumn("StateId");
+        public async Task<IEnumerable<Country>> GetAllCountriesAsync()
+        {
+            var query = @"SELECT CountryName, Code, IsBillingEnabled, IsShippingEnabled, 
+                          IsCityEnabled, IsZipCodeEnabled, IsDistrictEnabled, StateId
+                          FROM Companies";
+
+            return await _connection.QueryAsync<Country>(query);
+        }
+
+        public async Task<Country> GetCountryByCodeAsync(string companyCode)
+        {
+            var query = @"
+                SELECT 
+                    Code
+                FROM
+                    Companies
+                WHERE
+                    Code = @CountrCode";
+
+            return await _connection.QueryFirstOrDefaultAsync<Country>(query, new { CountryCode = companyCode });
+        }
+
+        public async Task<Country> GetCountryByNameAsync(string countryName)
+        {
+            var query = @"
+                SELECT 
+                    CountryName
+                FROM
+                    Companies
+                WHERE
+                    CountryName = @CompanyName";
+
+            return await _connection.QueryFirstOrDefaultAsync<Country>(query, new { CompanyName = countryName });
         }
     }
 }
