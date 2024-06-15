fa54e20d1921e84f099a563826b71cc06ff6fbe7
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-06-15 05:22:11 -0700

    feat(#1633): Vuilding messaging flow

---
+++ src/MessageBrokers/CloudSuite.Core.Messaging.RabbitMq/MessagingConsumer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Core.Messaging.RabbitMq
+{
+    public class MessagingConsumer
+    {
+        private readonly MessagingConfiguration _config;
+
+        public MessagingConsumer(MessagingConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void Start(Action<string>)
+    }
+}
